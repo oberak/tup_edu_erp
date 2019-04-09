0c82632025c51c83ae46d3ae507c64f8c0e25b54
Gilho Won <kiaora@Gilhos-MacBook-Pro.local>
2019-04-09 10:20:53 +0630

    Modify /web/signup   - remove depends   - add readonly

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web', 'base_setup', 'auth_oauth', 'auth_signup', 'education_core'],
+    'depends': ['base', 'auth_signup', 'education_core'],
 
     # always loaded
     'data': [
