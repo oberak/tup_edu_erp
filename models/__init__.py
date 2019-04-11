93feecd3eaa892248226c1ceeae8a8b3773a2848
Myat Thida Soe <myatthidasoe08@gmail.com>
2019-04-11 14:23:13 +0630

    Timetable/default(CurrentA.Y)

--- models/__init__.py
+++ models/__init__.py
@@ -5,4 +5,4 @@ from . import tup_education_application
 from . import tup_education_faculty
 from . import tup_hr_department
 from . import tup_education_subject
-# from . import tup_education_time_table
+from . import tup_education_time_table
