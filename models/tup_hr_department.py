66776773ef56d0ee8be7443c58c0e0d45c209495
Gilho Won <kiaora@Gilhos-MacBook-Pro.local>
2019-04-12 10:46:12 +0630

    Add unique constraint in hr.department

--- models/tup_hr_department.py
+++ models/tup_hr_department.py
@@ -9,3 +9,5 @@ class HrDepartment(models.Model):
     is_major = fields.Boolean(string="is Major", help="Tick if this department is a Major")
     can_enroll = fields.Boolean(string="Enrollment possible", help="Tick if this department can enroll")
     major_code =fields.Char('Major code', help="Major code")
+
+    _sql_constraints = [ ('major_code', 'unique (major_code)','Major code already exists for another Major!') ]
