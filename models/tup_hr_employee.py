76ba06204777d1de6410ada39f1a54d1b665c950
MinNyiThatTant <minnyithattant@gmail.com>
2019-04-23 13:53:25 +0630

    employee (add fields)

--- models/tup_hr_employee.py
+++ models/tup_hr_employee.py
@@ -6,4 +6,7 @@ class EmployeeCategory(models.Model):
     _description = "Employee"
     
 
-    
+    #add field
+    place_of_birth = fields.Char(string='Place Of Birth', required=True, help="Enter Place of Birth")
+    certificate_level = fields.Char(String='Certificate Level', required=True, help="Enter certificate level")
+    related_field = fields.Char(string='Related Field', required=False, help="Enter related field")
