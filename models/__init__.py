840d97a924996e3c470121f210b1553f00ade31d
WintSandar <wint@bnbb.co.kr>
2019-04-24 14:51:51 +0630

    Delete files
    - signup

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,3 @@ from . import education_core
 from . import education_promotion
 from . import education_time_table
 from . import hr_recruitment
-
-# TODO move to education_core
-from . import tup_education_student_signup
