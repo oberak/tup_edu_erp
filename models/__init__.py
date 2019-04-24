804d55989d1b7f00ad4218a74b14c3536153ab56
Gilho Won <kiaora@Gilhos-MacBook-Pro.local>
2019-04-24 09:37:45 +0630

    Delete models.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from . import models
 from . import tup_education_application
 from . import tup_education_faculty
 from . import tup_hr_department
 from . import tup_education_subject
+from .education_core import education_class_division
