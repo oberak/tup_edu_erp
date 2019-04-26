6fb43b63a39e81caa5534197957dce55db15a5f4
MinNyiThatTant <minnyithattant@gmail.com>
2019-04-26 13:31:03 +0630

    hr_employee (add submenu Paper Publication Record) #12

--- models/hr_recruitment/hr_employee.py
+++ models/hr_recruitment/hr_employee.py
@@ -16,6 +16,7 @@ class EmployeeCategory(models.Model):
     eye_color = fields.Char(string='Eye Color', help="Enter eye color")
     noticeable = fields.Char(string='Noticeable Remark', help="Enter height")
     complexion = fields.Char(string='Complexion', help="Enter complexion")
+    research_id = fields.Many2one('hr.paperpublication', string='Title Of Research Article', help="Enter Research Article")
     weight = fields.Char(string='weight', help="Enter weight")
     emergency_contact = fields.Char(string='Emergency Contact', required=True, help='Enter emergency contact')
     have_child = fields.Boolean(string="Have Children", default=False,
@@ -28,3 +29,22 @@ class EmployeeCategory(models.Model):
         ('five', 'Five'),
         ('above five', 'Above Five')
     ], string='Number of Children', default='one')
+    
+
+
+
+
+class TupPaperPublicationRecord(models.Model):
+    _name = 'hr.paperpublication'
+    _description = 'Paper Publication Record'
+    _inherit = ['mail.thread']
+
+    #new fields
+    title_of_research_article = fields.Char(string='Title Of Research Article', required=True, help="Enter Research Article")
+    author_id = fields.Many2one('hr.employee', string='Author Name', required=True, help="Enter Author Name")
+    rel_sub = fields.Char(string='Related Subject Specialization', required=True, help="Enter Related Subject Specification")
+    publicated_date = fields.Date(
+        'Publicated Date', required=True, default=lambda self: fields.Date.today(),
+        track_visibility="onchange") 
+    # vol_no = fields.Char(string='Vol No:', required=True, help="Enter Paper Volume Number")
+    # page_range = fields.Integer(string='Page_range', required=True, help="Enter Page range")
