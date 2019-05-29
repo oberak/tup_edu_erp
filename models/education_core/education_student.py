f940d8e090ede74c40015df32ad1029013660e7a
--add <thidalwin.it@gmail.com>
2019-05-29 16:38:06 +0630

    update education_student  - remove field "name"

--- models/education_core/education_student.py
+++ models/education_core/education_student.py
@@ -70,8 +70,7 @@ class EducationStudent(models.Model):
             receipt_ids = self.env['account.invoice'].search([('student_id', '=', rec.id), ('state', '!=', 'cancel')])
             rec.receipt_count = len(receipt_ids)
 
-    #add field 
-    name = fields.Char('Name')
+    #add fields
     nrc_no = fields.Char(string='NRC Number', required=True, help="Enter NRC Number of Student")
     is_registered = fields.Boolean(string="Check Signup", default=False)
     student_id=fields.Char(string='Student ID.',  help="Enter Student ID of Student")
