848372da0f68aedbf0e715e8144e6e920d8f7500
--add <thidalwin.it@gmail.com>
2019-06-14 12:45:58 +0630

    update  - monthly attendance and overall attendance  -  set access right to faculty and faculty_classes  - set access right to employee

--- models/education_attendances/student_overall_attendances.py
+++ models/education_attendances/student_overall_attendances.py
@@ -15,8 +15,10 @@ class EducationStudentsAttendance(models.Model):
                           ('09', 'September'), ('10', 'October'), ('11', 'November'), ('12', 'December'), ], 
                           string='Month')
     stu_m_attendances = fields.Float('Monthly Attendance')
+    total_attendances= fields.Float('Attendance per Month', default = 1.0)
     m_attendances_percent = fields.Float('Monthly Attendance (%) ')
-    
+    ay_id = fields.Many2one('education.academic.year',string='Academic Year',default=lambda self: self.env['education.academic.year']._get_current_ay())
+    oatt_id = fields.Many2one('education.overall.attendances',string='Overall Attendance ID')
 
     @api.onchange('class_division')
     def _onchange_class(self):
@@ -31,23 +33,24 @@ class EducationStudentsAttendance(models.Model):
         }
         return vals
   
-    @api.onchange('month')
+    @api.onchange('month','total_attendances')
     def _onchange_monthly_attendance(self):
         #this_semester = self.env['education.semester']._get_current_semester()
+        att= False
         m_att = 0.0
+        att_per = 0.0
         for rec in self:
-            obj = self.env['education.attendances.line'].search([('student_id','=',rec.student_id.id),('class_division','=',rec.class_division.id)])
+            obj = self.env['education.attendances.line'].search([('student_id','=',rec.student_id.id),('class_division','=',rec.class_division.id),('remark','=',True)])
             for dat in obj:
                 month = datetime.strftime(datetime.strptime(dat.date, "%Y-%m-%d"), "%m")
-                print(month,'>>>>>>>>>>>>>>>>>>>>>>>>>>')
-                if month == self.month:
-                    att = self.env['education.attendances.line'].search([('student_id','=',rec.student_id.id),('date','=',dat.date)])
-                    for att_id in att :
-                        if att_id.remark == True :
-                            m_att += att_id.hours
+                if month == self.month:              
+                    att = self.env['education.attendances.line'].search([('id','=',dat.id),('student_id','=',rec.student_id.id),('date','=',dat.date)])                   
+                    for att_id in att :                 
+                        m_att += att_id.hours                            
             rec.stu_m_attendances= m_att
-         
-       
+            att_per = rec.stu_m_attendances * 100 /rec.total_attendances
+            rec.m_attendances_percent = att_per
+
     # Naming Class/Attendance_date
     def get_name(self):
         """To generate name for the model"""
@@ -92,13 +95,16 @@ class EducationStudentsAttendance(models.Model):
 class EducationStudentsOverallAttendance(models.Model):
     _name = 'education.overall.attendances'
 
+    ay_id = fields.Many2one('education.academic.year',string='Academic Year',default=lambda self: self.env['education.academic.year']._get_current_ay())
     name = fields.Char(compute='get_name', string='Overall Attendance', default='New')
     class_division = fields.Many2one('education.class.division', string='Class', required=True, 
         domain=lambda self: [('academic_year_id', '=', self.env['education.academic.year']._get_current_ay().id)])
     student_id = fields.Many2one('education.student', string='Student')
+    total_attendances= fields.Float('Attendance per Academic Year')
     stu_overall_percent = fields.Float('Overall Attendance (%)')    
     stu_overall_attendances = fields.Float('Overall Attendance')
     state = fields.Selection([('draft', 'Draft'), ('done', 'Done')],string='State', default='draft', track_visibility='onchange')
+    matt_ids = fields.One2many('education.student.attendances','oatt_id',string='Monthly Attendance Ids')
 
     @api.onchange('class_division')
     def _onchange_class(self):
@@ -120,23 +126,23 @@ class EducationStudentsOverallAttendance(models.Model):
             i.name = str(i.class_division.name)+ "/" + str(i.student_id.name)+ "/ Overall Attendances" 
 
     def overall_attendance(self):
-        all_attends = 0.0
-        overall_att = 0.0
-        this_semester = self.env['education.semester']._get_current_semester()
-        timetable = self.env['education.timetable'].search([('semester', '=', this_semester.id), ('class_division', '=', self.class_division.id)])
-        schu = self.env['education.timetable.schedule'].search([('timetable_id','=',timetable.id),('class_division','=',self.class_division.id)])
-        for all_att in schu:
-            all_attends += all_att.hours
-        all_attends = all_attends
-        
+        this_ay = self.env['education.academic.year']._get_current_ay()      
+        overall_att = 0.0        
         for rec in self:    
-            obj = self.env['education.student.attendances'].search([('class_division','=',self.class_division.id),('student_id','=',self.student_id.id)])           
-            for o_att in obj:
-                overall_att += o_att.stu_m_attendances
-            rec.stu_overall_attendances= overall_att 
-        print('all attendance >>>>>>>>',all_attends)
-        self.stu_overall_percent = overall_att * 100 / all_attends
-        self.state = 'done'     
+            obj = self.env['education.student.attendances'].search([('student_id','=',rec.student_id.id),('class_division','=',self.class_division.id),('ay_id','=',this_ay.id)])
+            if obj :
+                for m_att in obj:                    
+                    overall_att += m_att.stu_m_attendances
+                    m_att.oatt_id = self.id
+                    oatt = m_att.oatt_id
+                    data ={
+                        'oatt_id' : oatt.id,
+                    }
+                    self.env['education.student.attendances'].update(data)                    
+            rec.stu_overall_attendances= overall_att        
+            rec.stu_overall_percent = rec.stu_overall_attendances * 100 / rec.total_attendances
+            rec.state = 'done' 
+            
 
     @api.model
     def create(self, vals):
