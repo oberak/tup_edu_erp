7d1cd8416df700cbebdf7dffb7542fc309cbcf16
--add <thidalwin.it@gmail.com>
2019-07-02 11:25:38 +0630

    update hr_department form
    1. change name
        - major code to department code
        - major id to department id

--- models/hr_recruitment/hr_department.py
+++ models/hr_recruitment/hr_department.py
@@ -8,8 +8,8 @@ class HrDepartment(models.Model):
 
     is_major = fields.Boolean(string="is Major", help="Tick if this department is a Major")
     can_enroll = fields.Boolean(string="Enrollment possible", help="Tick if this department can enroll")
-    major_code =fields.Char('Major code', help="Major code")
-    major_id =fields.Char('Major ID', help='Major ID like 10')
+    major_code =fields.Char('Department code', help="Department code")
+    major_id =fields.Char('Department ID', help='Department ID like 10')
 
     _sql_constraints = [ ('major_code_uniq', 'unique (major_code)','Major code already exists for another Major!') ,
                          ('major_id_uniq', 'unique(major_id)', "Major ID already exists for another Major!")]
